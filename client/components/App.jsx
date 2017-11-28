d1afc5092b5a23f52710875da13ccfcf8f185eb5
Samuel Kwak <kwak123@gmail.com>
2017-11-28 10:37:52 -0500

    further backend setup

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.onClick = this.onClick.bind(this);
   }
+
+  onClick() {
+    axios.get('/test')
+    .then((data) => console.log('fetched'))
+    .catch((error) => console.log('failed'));
+  }
+
 
   render() {
     return (
       <div>
         Hello
+        <button onClick={this.onClick}>Click</button>
       </div>
     );
   }
--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var axios = require('axios');
+var protobuf = require('protobufjs');
 
 var app = express();
 
@@ -9,8 +11,27 @@ var logger = (req, res, next) => {
 };
 
 app.use(logger);
-app.use(bodyParser.json());
+// app.use(bodyParser.raw());
 app.use(express.static(__dirname + '/dist'));
+
+app.get('/test', (req, res) => {
+  var decoder;
+  protobuf.load(`${__dirname}/lib/proto/nyct-subway.proto`)
+  .then((root) => {
+    decoder = root.lookup('transit_realtime');
+    console.log(decoder);
+  })
+  .then(() => {
+    return axios.get('http://datamine.mta.info/mta_esi.php?key=b9562b987b663ac9940c5d42f1194cad');
+  })
+  .then((data) => {
+    // let test = decoder.decode(data);
+    console.log(test);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+});
 
 var port = process.env.port || 3000;
 app.listen(port, () => console.log(`now listening on ${port}`));
