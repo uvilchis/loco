7f4d9c2387873669ca4b18ce3db95efe27187178
Christine Kong <ckong1792@gmail.com>
2017-11-29 11:07:54 -0500

    made trainline component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import axios from 'axios';
 
-class App extends React.Component {
+export default class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      trains: []
+    }
     this.onClick = this.onClick.bind(this);
   }
 
@@ -17,11 +20,14 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        Hello
+        Hello and welcome to loco, your one stop resource for MTA delays
+        <div className="train-lines">
+          {this.state.trains.map((line) => {
+            return <TrainLine />
+          })}
+        </div>
         <button onClick={this.onClick}>Click</button>
       </div>
     );
   }
 }
-
-export default App;
