fd2eea66f51d6a5c466a565a7ae4ddedc6347812
Uciel <uciel777@Uciels-MacBook-Pro.local>
2017-12-01 11:43:33 -0500

    Conditional Render Running

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import axios from 'axios';
 import TrainLine from './TrainLine.jsx';
 import mockData from '../mockservice.json';
+import Details from './Details.jsx';
+
 
 export default class App extends React.Component {
   constructor(props) {
@@ -9,9 +11,11 @@ export default class App extends React.Component {
     this.state = {
       trains: [],
       stops: [],
-      user: null
+      user: null,
+      displayed : 'main'
     };
     this.onClick = this.onClick.bind(this);
+    this.setAppState = this.setAppState.bind(this);
   }
 
   componentDidMount() {
@@ -20,31 +24,43 @@ export default class App extends React.Component {
   }
 
   onClick() {
-    // axios.get('/test')
-    // .then((data) => console.log('fetched', data))
-    // .catch((error) => console.log('failed'));
+    axios.get('/routes')
+    .then((data) => {
+      console.log('fetched', data)
+      this.setState({trains : data.data})
+    })
+    .catch((error) => console.log('failed', error));
+  }
+
+  setAppState(input) {
+    this.setState({displayed: input })
   }
 
   render() {
-    return (
-      <div>
-        <div className="navbar">
-          <div className="logo_container">
-            <h1 className="logo">Loco</h1>
+    return this.state.displayed === 'main' ? (
+        <div>
+          <div className="navbar">
+            <div className="logo_container">
+              <h1 className="logo">Loco</h1>
+            </div>
+          </div>
+          <div className="trainline">
+            <h3 className="trainline_header">Train Status</h3>
+            <div className="trainline_container">
+              {this.state.trains.map((line, idx) =>
+                <TrainLine
+                  line={line}
+                  key={idx}
+                  loggedIn={this.state.user ? true : false}
+                  setAppState={this.setAppState}
+                  />
+
+              )}
+            </div>
           </div>
         </div>
-        <div className="trainline">
-          <h3 className="trainline_header">Train Status</h3>
-          <div className="trainline_container">
-            {this.state.trains.map((line, idx) => 
-              <TrainLine 
-                line={line}
-                key={idx} 
-                loggedIn={this.state.user ? true : false} />
-            )}
-          </div>
-        </div>
-      </div>
-    );
+      ) : (
+        <Details />
+      );
   }
 }
