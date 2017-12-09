d6116a7706c4d556502f33d77ef42ef98cfe1e26
Uciel <uciel777@Uciels-MacBook-Pro.local>
2017-12-09 17:05:26 -0500

    Main and Nav now working off API requests. SIR nav & details page fixed.

--- server/controllers/realtime.js
+++ server/controllers/realtime.js
@@ -1,22 +1,46 @@
-const instance = require('../instance');
+// const instance = require('../instance');
+const axios = require('axios')
+
+const instance = axios.create({
+  baseURL : 'http://ec2-18-221-253-159.us-east-2.compute.amazonaws.com:3001'
+})
+
 
 const getServiceData = (req, res) => {
-  let data = instance.getServiceData();
-  if (data) {
-    res.send(data);
-  } else {
-    res.sendStatus(404);
-  }
+  instance.get('/loco/service', {
+    params : { sub: 'mta'}
+  })
+  .then(response => {
+    res.send(response.data)
+  })
+  .catch(err => res.sendStatus(404))
+  // if (data) {
+  //   res.send(data);
+  // } else {
+  //   res.sendStatus(404);
+  // }
 };
 
 const getServiceRouteData = (req, res) => {
   let routeId = req.params.route_id;
-  let data = instance.getServiceRouteData(routeId);
-  if (data) {
-    res.send(data);
-  } else {
-    res.sendStatus(404);
-  }
+  //let data = instance.getServiceRouteData(routeId);
+  instance.get(`/loco/service/route`, {
+    params : {
+      sub: 'mta',
+      route_id : `${routeId}`
+    }
+  })
+  .then(response => {
+    res.send(response.data)
+  })
+  .catch(err => {
+    console.log('error', err.data)
+  })
+  // if (data) {
+  //   res.send(data);
+  // } else {
+  //   res.sendStatus(404);
+  // }
 };
 
 module.exports = {
